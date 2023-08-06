1acda7b3f11d1e26ea0cfae06359054a385528a0
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2023-08-06 00:06:26 -0600

    initial set up

---
+++ dbcon.php
@@ -0,0 +1,16 @@
+<?php
+
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "sxnarrucia");
+    define("PASSWORD", "Fundamentos1!");
+    define("DATABASE", "CRUD_Sistemas");
+
+    $connection = mysqli_connect(HOSTNAME,USERNAME,PASSWORD,DATABASE);
+    
+    if (!$connection) {
+        die("Connection Failed");
+    } else {
+        echo("yesss");
+    }
+    
+?>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Gestión de Usuarios</h1>
+    <div class="containter">
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Primer Nombre</th>
+                <th>Apellidos</th>
+                <th>Cédula</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>3</td>
+                <td>Esteban</td>
+                <td>Sanarrucia</td>
+                <td>24</td>
+            </tr>
+            <tr>
+                <td>6</td>
+                <td>Rebeca</td>
+                <td>Sanarrucia</td>
+                <td>14</td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+</html>
