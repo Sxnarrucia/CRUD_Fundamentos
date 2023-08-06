726d66d3703d398bed62e3f61c9bd529e7baf79a
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2023-08-06 00:30:47 -0600

    sql test

--- index.php
+++ index.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-</head>
-<body>
-    <h1>Gestión de Usuarios</h1>
-    <div class="containter">
-    <table>
+    <?php include('header.php'); ?>
+    <?php include('dbcon.php'); ?>
+
+        <h2>USUARIOS</h2>
+        <table class="table table-hover table-bordered table-striped">
         <thead>
             <tr>
                 <th>ID</th>
@@ -19,6 +12,20 @@
             </tr>
         </thead>
         <tbody>
+            <?php
+
+            $query = "select * from 'Usuarios'";
+
+            $result = mysqli_query($connection, $query);
+
+            if (!$result) {
+                die("Fallo en la solicitud".mysqli_error());
+            } else {
+                print_r($result);
+            }
+            
+
+            ?>
             <tr>
                 <td>3</td>
                 <td>Esteban</td>
@@ -32,8 +39,5 @@
                 <td>14</td>
             </tr>
         </tbody>
-    </table>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-</body>
-</html>
+        </table>
+        <?php include('footer.php'); ?>
