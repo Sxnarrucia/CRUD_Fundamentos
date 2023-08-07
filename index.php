14e3669b2a61be392265dd4160c885c55f09c293
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2023-08-06 22:51:18 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <div class="box1">
 <h2>USUARIOS</h2>
-<button class="btn btn-primary">Agregar Usuarios</button>
+<button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Agregar Usuarios</button>
 </div>
 <table class="table table-hover table-bordered table-striped">
     <thead>
@@ -40,4 +40,41 @@
 
     </tbody>
 </table>
+
+<form>
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Agregar Usuarios</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+            <div class="form-group">
+                <label for="f_name">Primer Nombre</label>
+                <input type="text" name="f_name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="f_name">Segundo Nombre</label>
+                <input type="text" name="l_name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="f_name">Identificación</label>
+                <input type="text" name="id" class="form-control">
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-success">Agregar</button>
+      </div>
+    </div>
+  </div>
+</div>
+</form>
+
 <?php include('footer.php'); ?>
