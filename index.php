6909cec8af09b0b8bb02899c1e72ab172d7ecc77
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2023-08-06 23:38:24 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
 </table>
 
 
-<form action="insert_data.php" method="post">
+<form action="insert_data.php" method="POST">
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
