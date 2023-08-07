4374efcccee22950923ad1aa052733b18482a296
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2023-08-06 21:59:05 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
-<?php
-$servername = "localhost";
-$username = "sxnarrucia";
-$password = "Fundamentos1!";
-$dbname = "CRUD_Sistemas";
+<?php include('header.php'); ?>
+<?php include('dbcon.php'); ?>
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
+<h2>USUARIOS</h2>
+<table class="table table-hover table-bordered table-striped">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Primer Nombre</th>
+            <th>Apellidos</th>
+            <th>Cédula</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
 
-$sql = "SELECT * FROM Usuarios";
-$result = mysqli_query($conn, $sql);
+        $query = "SELECT * FROM Usuarios";
 
-if (mysqli_num_rows($result) > 0) {
-  // output data of each row
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "id: " . $row["ID"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
+        $result = mysqli_query($connection, $query);
 
-mysqli_close($conn);
-?>
+        if (!$result) {
+            die("Fallo en la solicitud".mysqli_connect_error());
+        } else {
+            print_r($result);
+        }
+
+        ?>
+        <tr>
+            <td>3</td>
+            <td>Esteban</td>
+            <td>Sanarrucia</td>
+            <td>24</td>
+        </tr>
+        <tr>
+            <td>6</td>
+            <td>Rebeca</td>
+            <td>Sanarrucia</td>
+            <td>14</td>
+        </tr>
+    </tbody>
+</table>
+<?php include('footer.php'); ?>
