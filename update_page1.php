530cf24e09eea32c0316f2b1313e573ec3215d50
Esteban Sanarrucia <123785378+Sxnarrucia@users.noreply.github.com>
2023-08-12 23:33:36 -0600

    Add files via upload

--- update_page1.php
+++ update_page1.php
@@ -24,8 +24,31 @@ if (isset($_GET['id'])) {
 }
 ?>
 
+<?php
+    if (isset($_POST['update_users'])) {
 
-            <form>
+        if (isset($_GET['id_new'])) {
+            $idnew = $_GET['id_new'];
+        }
+
+        $fname = $_POST['f_name'];
+        $lname = $_POST['l_name'];
+        $identification = $_POST['identification'];
+
+        $query = "UPDATE Usuarios SET First_Name = '$fname', Last_Name = $lname, Identification = $identification WHERE ID = '$idnew'";
+
+        $result = mysqli_query($connection, $query);
+
+   if (!$result) {
+       die("Fallo en el query".mysqli_error($connection));
+    } else {
+        header('location:index.php?update_msg=Actualizado Correctamente');
+    }
+    }
+?>
+
+
+            <form action="update_page1.php?id_new=<?php echo $id; ?>" method="POST">
             <div class="form-group">
                 <label for="f_name">Primer Nombre</label>
                 <input type="text" name="f_name" class="form-control" value="<?php echo $row['First_Name'] ?>">
